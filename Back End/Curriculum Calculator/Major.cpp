22f1ff0674ff59dc481b995d89c5da352071d10e
Saianeesh Keshav Haridas <saianeeshkh@gmail.com>
2017-01-13 13:03:39 -0500

    Added accessor functions

--- Back End/Curriculum Calculator/Major.cpp
+++ Back End/Curriculum Calculator/Major.cpp
@@ -16,3 +16,33 @@ Major::Major(int dep, int uni, int[] req, int[][] choice, int[] shit, int[] dist
   shiz = shit;
   distReq = dist;
 }
+
+int Major::getDepartment()
+{
+  return department;
+}
+
+int Major::getUniversity()
+{
+  return university;
+}
+
+int* Major::getReqCourses()
+{
+  return reqCourses;
+}
+
+int* Major::getChoiceCourses()
+{
+  return choiceCourses;
+}
+
+int* Major::getShiz()
+{
+  return shiz;
+}
+
+int* Major::getDistReq()
+{
+  return distReq;
+}
