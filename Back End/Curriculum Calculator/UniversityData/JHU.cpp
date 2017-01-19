e24cbcd036f456c29082beb71d71b06a8f302a91
Saianeesh Keshav Haridas <saianeeshkh@gmail.com>
2017-01-18 20:19:02 -0500

    Gave the AMS Major the Optimization and Operations Research focus area

--- Back End/Curriculum Calculator/UniversityData/JHU.cpp
+++ Back End/Curriculum Calculator/UniversityData/JHU.cpp
@@ -9,8 +9,9 @@ void main()
   std::vector<int> opt3 {110201, 110212};
   std::vector<int> opt4 {110302, 110306, 110417, 550386, 550388, 550391};
   std::vector<int> opt5 {171426, 250205, 550200, 510202, 530371, 540305, 550281, 550383, 550385, 550386, 550383, 550400, 550413, 550415, 550433, 550436, 550443, 550450, 550487, 550493, 560220, 570210, 580200, 580223, 600475};
-  std::vector<std::vector<int>> opt {opt1, opt2, opt3, opt4, opt5};
-  std::vector<int> shit {122000010, 318000010, 112001000};
+  std::vector<int> opt6 {550362,550400,550453,550463}
+  std::vector<std::vector<int>> opt {opt1, opt2, opt3, opt4, opt5, opt6};
+  std::vector<int> shit {2, 122000010, 318000010, 112001000};
   std::vector<int> dist;
-  Major* AMSBS = new Major::Major(550, 0, req, opt, shit, dist, true, 0);
+  Major* AMSBSOOR = new Major::Major(550, 0, req, opt, shit, dist, true, 3);
 }
