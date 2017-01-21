6f95c3678fb0edba8c2580dd4c36751e17f342b0
venariam <bbao2@jhu.edu>
2017-01-20 21:57:26 -0500

    Added required, see important comment
    Is it really necessary to have a student class? It seems like these variables and  functions would almost be the major components of the main() of the program, and it doesn't seem like we'll be creating more than 1 "Student" object.

--- Back End/Curriculum Calculator/Student.h
+++ Back End/Curriculum Calculator/Student.h
@@ -5,6 +5,7 @@ class Student
   int university;
   std::vector<Major> majors;
   std::vector<int> taken;
+  std::vector<std::vector<int>> required;
   public:
     Student(int);
     int getUniversity();
@@ -14,4 +15,6 @@ class Student
     std::vector<int> getTaken();
     void addTaken(int);
     void removeTaken(int);
+    void calculateRequired();
+    std::vector<std::vector<int>> getRequired();
 };
