5062ce20479e404846813ba47d9a2bbc70a94444
venariam <bbao2@jhu.edu>
2017-01-21 18:17:44 -0500

    Update Major.h

--- Back End/Curriculum Calculator/Major.h
+++ Back End/Curriculum Calculator/Major.h
@@ -3,18 +3,18 @@
 class Major
 {
   int department, university, focusArea;
-  std::vector<int> reqCourses;
-  std::vector<std::vector<int>> choiceCourses;
-  std::vector<int> shiz;
+  std::vector<long> reqCourses;
+  std::vector<std::vector<long>> choiceCourses;
+  std::vector<long> shiz;
   int degree;                   //Distinguishes between type of degree: 0 for B.A, 1 for B.S., more to add later
   int reqCredits;
   public:
-    Major(int, int, std::vector<int>, std::vector<std::vector<int>>, std::vector<std::vector<int>>, int, int, int);
+    Major(int, int, std::vector<long>, std::vector<std::vector<long>>, std::vector<std::vector<long>>, int, int, int);
     int getDepartment();
     int getUniversity();
-    std::vector<int> getReqCourses();
-    std::vector<std::vector<int>> getChoiceCourses();
-    std::vector<std::vector<int>> getShiz();
+    std::vector<long> getReqCourses();
+    std::vector<std::vector<long>> getChoiceCourses();
+    std::vector<std::vector<long>> getShiz();
     int getDegree();
     int getFocusArea();
     int getReqCredits();
