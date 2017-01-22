c1ec3f8b8a53dd257c91d9561d32c6358b5f90a6
venariam <bbao2@jhu.edu>
2017-01-21 20:16:48 -0500

    Update Student.cpp

--- Back End/Curriculum Calculator/Student.cpp
+++ Back End/Curriculum Calculator/Student.cpp
@@ -66,6 +66,7 @@ void calculateRequired()
   required.clear();
   requiredCredits = 0;
   bool tempValue = false;
+  bool tempValue2 = false;
   std::vector<long> FML;
   vector
   for(int i = 0 ; i < majors.size() ; i++)
@@ -76,6 +77,7 @@ void calculateRequired()
     }
     for (int j = 0 ; j < majors[i].getReqCourses().size() ; j++)
     {
+		tempValue = false;	
       for (int k = 0 ; k < required[0].size() ; k++)
       {
         if (majors[i].getReqCourses()[j] == required[0][k])
@@ -98,6 +100,10 @@ void calculateRequired()
 				if (majors[i].getReqCourses()[j] == required[k][l])
 				{
 					required[k][1] -= ((((required[k][l] / 100000000)% 10) * 10) + ((required[k][l] / 10000000)% 10)); 
+					if (required[k][1] < 0)
+					{
+					 required[k][1] = 0;
+					}
 					required[k].erase(l); //must change above if course id integer changes from 15: divide by 10 again if add 1 digit, multiple by 10 if subract 1 digit
 				}
 				else
@@ -124,6 +130,10 @@ void calculateRequired()
 				|| (required[k][l]%10 == majors[i].getReqCourses()[j]%10))
 				{
 					required[k][1] -= ((((required[k][l] / 100000000)% 10) * 10) + ((required[k][l] / 10000000)% 10));
+					if (required[k][1] < 0)
+					{
+					 required[k][1] = 0;
+					}
 				}
 			}
 			if (required[k][1] == 0)
@@ -133,15 +143,110 @@ void calculateRequired()
 		 }
 		 else if (required[k][0] == 3)
 		 {
+		    FML.clear();
+			FML = required[k];
+			FML[0] = - 1;
+			k += FML.size();
+			for (int z = FML[1] + k; z < FML.back() + k;z++)
+			{
+		if (required[z][0] == 1)
+         {
 			
+			for (int l = 2 ; l < required[z].size() ;)
+			{
+				if (majors[i].getReqCourses()[j] == required[z][l])
+				{
+					required[z][1] -= ((((required[z][l] / 100000000)% 10) * 10) + ((required[z][l] / 10000000)% 10)); 
+					
+					if (required[z][1] < 0)
+					{
+					 required[z][1] = 0;
+					}
+					required[z].erase(l); //must change above if course id integer changes from 15: divide by 10 again if add 1 digit, multiple by 10 if subract 1 digit
+				}
+				else
+				{
+					l++;
+				}
+			}
+         }
+         else if (required[z][0] == 2)
+		 {
+			for (int l = 2 ; l < required[z].size() ; l++)
+			{
+				if((required[z][l]/10000000000 <= (majors[i].getReqCourses()[j]/100000000000)%10 && 
+				(required[z][l]/1000000000)%10 <= (((majors[i].getReqCourses()[j]/100000000)%10) * 10 ) && 
+				((((required[z][l][j] / 100000000)%10)*100) + (((required[z][l] / 10000000)%10)*10) + (required[z][l] / 1000000)%10) ==
+				((((majors[i].getReqCourses()[j] / 100000000000000)%10)*100) + (((majors[i].getReqCourses()[j] / 10000000000000)%10)*10)
+				+ (majors[i].getReqCourses()[j] / 1000000000000)%10)) || ((required[z][l]/100000)%10 == (majors[i].getReqCourses()[j]/100000)%10)
+				|| ((required[z][l]/10000)%10 == (majors[i].getReqCourses()[j]/10000)%10) || ((required[z][l]/1000)%10 == (majors[i].getReqCourses()[j]/1000)%10)
+				|| ((required[z][l]/100)%10 == (majors[i].getReqCourses()[j]/100)%10) || ((required[z][l]/10)%10 == (majors[i].getReqCourses()[j]/10)%10)
+				|| (required[z][l]%10 == majors[i].getReqCourses()[j]%10))
+				{
+					required[z][1] -= ((((required[z][l] / 100000000)% 10) * 10) + ((required[z][l] / 10000000)% 10));
+					
+					if (required[z][1] < 0)
+					{
+					 required[z][1] = 0;
+					}
+				}
+			}
+		 }
+			}
+			for (int z = k; z < FML.size() + k - 1; z++)
+			{
+				tempValue2 = false;
+				for (int y = FML[z] + 1 + k ; y <= FML[z+1] + k ; y++)
+				{
+					if(required[y][1] != 0)
+					{
+						tempValue2 = true;
+					}
+					if(tempValue2 == false)
+					{
+					break;
+					}
+				}
+				if(tempValue2 == false)
+				{
+				break;
+				}
+			}
+			if(tempValue2 == false)
+			{
+				for(int z = FML.back() + k ;z > FML[0] + k;z--)
+				{
+					required.erase(z);
+				}
+			}
+			else
+			{
+				for(int z = FML.back() + k ;z > FML[0] + k + 1;z--)
+				{
+					if (required[z][1] == 0)
+					{
+						required.erase(z);
+						for (int y = 1 ; y < FML.size() ; y++)
+						{
+							if(FML[y] > z)
+							{
+								required[k][y]--;
+							}
+						}
+					}
+				}		
+			}
 		 }
        }
     }
 	for (int j = 0 ; j < majors[i].getReqCourses().size() ; j++)
 	{
+	
+	}
+	for (int j = 0 ; j < majors[i].getReqCourses().size() ; j++)
+	{
 		
 	}
-	
   }
 }
 
