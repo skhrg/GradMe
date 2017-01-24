d27b223e0d5a7d33877d26ceb4945d51085c5d38
venariam <bbao2@jhu.edu>
2017-01-24 01:36:17 -0500

    Added resident requirement

--- Back End/Curriculum Calculator/Major.h
+++ Back End/Curriculum Calculator/Major.h
@@ -2,15 +2,14 @@
 
 class Major
 {
-  int department, university, focusArea;
+  int department, university, focusArea, reqCredits, residentCredits;
   std::vector<long> reqCourses;
   std::vector<std::vector<long>> choiceCourses;
   std::vector<std::vector<long>> shiz;
   std::vector<std::vector<long>> option;
   int degree;                   //Distinguishes between type of degree: 0 for B.A, 1 for B.S., more to add later
-  int reqCredits;
   public:
-    Major(int, int, std::vector<long>, std::vector<std::vector<long>>, std::vector<std::vector<long>>, std::vector<std::vector<long>>, int, int, int);
+    Major(int, int, std::vector<long>, std::vector<std::vector<long>>, std::vector<std::vector<long>>, std::vector<std::vector<long>>, int, int, int, int);
     int getDepartment();
     int getUniversity();
     std::vector<long> getReqCourses();
@@ -20,4 +19,5 @@ class Major
     int getDegree();
     int getFocusArea();
     int getReqCredits();
+    int getResidentCredits();
 };
