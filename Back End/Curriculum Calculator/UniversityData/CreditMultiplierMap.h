1aa42160970f65640ce4d39073c5a54896aaf0f2
venariam <bbao2@jhu.edu>
2017-01-27 14:50:47 -0500

    Create CreditMultiplierMap.h
    File for credit multiplying factor

---
+++ Back End/Curriculum Calculator/UniversityData/CreditMultiplierMap.h
@@ -0,0 +1,17 @@
+//This file includes a map for the multiplier for all credits for a university in the data
+//This is so that we can store credit numbers as an integer while accomodating for programs that use half credits (or even 1/3, 1/4).
+//When taken up to the front-end, the displayed credits should all be divided by the mapped number for the cooresponding university
+//Divide the displayed credits for all course credits AND major required credits 
+//alternatively, this info can be stored elsewhere but not in this program's scope.
+//For instance, if the value is 2, divide all values to be displayed by 2 because it is currently being multiplied by a factor of 2
+
+//Please check this file before you enter any course ID data.
+
+#include <map>
+
+map<int, int>     //University ID, multiplier
+{
+
+{0000, 2},        //The Johns Hopkins University
+
+};
