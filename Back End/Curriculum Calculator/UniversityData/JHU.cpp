25e5f85c43845004419eb7bfcf01632f2ad784a1
venariam <bbao2@jhu.edu>
2017-01-27 14:34:41 -0500

    Update JHU.cpp

--- Back End/Curriculum Calculator/UniversityData/JHU.cpp
+++ Back End/Curriculum Calculator/UniversityData/JHU.cpp
@@ -22,7 +22,7 @@ void main()
   std::vector<int> opt5 {171426, 250205, 550200, 510202, 530371, 540305, 550281, 550383, 550385, 550386, 550383, 550400, 550413, 550415, 550433, 550436, 550443, 550450, 550487, 550493, 560220, 570210, 580200, 580223, 600475};
   std::vector<int> opt6 {550362,550400,550453,550463}
   std::vector<std::vector<int>> opt {opt1, opt2, opt3, opt4, opt5, opt6};
-  std::vector<int> shit {2, 122000010, 318000010, 112001000};*/
+  std::vector<int> shit {2, 122000010, 318000010, 112001000};
   Major* AMSBSOOR = new Major::Major(550, 0, req, opt, shit, 1, 3);
-  jhuMajors[55013] = AMSBSOOR;
+  jhuMajors[55013] = AMSBSOOR;*/
 }
