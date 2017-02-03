e6497c9551c607cc2f5d3be268523b3737f16f49
lylpasko <lyle.paskowitz@gmail.com>
2017-02-02 22:04:00 -0500

    Added carousel slider to main page.

--- Front End/assets/js/main.js
+++ Front End/assets/js/main.js
@@ -57,3 +57,18 @@ $(document).ready(function() {
     });
 
 });
+/* for carousel */
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel({
+    navigation : true, 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem: true,
+      pagination: false,
+      rewindSpeed: 500
+  });
+ 
+});
+
+
