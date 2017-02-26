ac727e4aaef74bad414addeacb48370c65048837
lylpasko <lyle.paskowitz@gmail.com>
2017-02-26 11:26:10 -0500

    banner update

--- Front End/assets/js/main_main.js
+++ Front End/assets/js/main_main.js
@@ -54,7 +54,30 @@ $(document).ready(function() {
 });
 
 
-/* for carousel */
+/* for navi */
+$(document).ready(function() {
 
+  var active1 = false;
+  var active2 = false;
+  var active3 = false;
+  var active4 = false;
+
+    $('.parent2').on('mousedown touchstart', function() {
+    
+     if (!active1) $(this).find('.test1').css({'transform': 'translate(-125px,70px)'});
+    else $(this).find('.test1').css({'transform': 'none'}); 
+     if (!active2) $(this).find('.test2').css({'transform': 'translate(-45px,110px)'});
+    else $(this).find('.test2').css({'transform': 'none'});
+      if (!active3) $(this).find('.test3').css({'transform': 'translate(45px,110px)'});
+    else $(this).find('.test3').css({'transform': 'none'});
+      if (!active4) $(this).find('.test4').css({'transform': 'translate(125px,70px)'});
+    else $(this).find('.test4').css({'transform': 'none'});
+    active1 = !active1;
+    active2 = !active2;
+    active3 = !active3;
+    active4 = !active4;
+      
+    });
+});
 
 
