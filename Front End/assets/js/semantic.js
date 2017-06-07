4cdfb1d4545945dc5908b4391e5f69c29ad127a0
lylpasko <lyle.paskowitz@gmail.com>
2017-06-07 15:03:34 -0400

    completed form functionality

--- Front End/assets/js/semantic.js
+++ Front End/assets/js/semantic.js
@@ -27,33 +27,14 @@ $(document).ready(function() {
     $(".btn").css({
       color: getProductColor
     });
+
+    $(".label").css({
+      "background-color": getProductColor + "!important"
+    });
   }
 
   var productItem = $(".product"),
     productCurrentItem = productItem.filter(".active");
-/* filter method selects elements via selector */
-  
-  $("#next").on("click", function(e) {
-    e.preventDefault();
-
-    var nextItem = productCurrentItem.next();
- /* next method finds next match for selector defined in productCurrentItem variable */  
-    productCurrentItem.removeClass("active");
-
-    if (nextItem.is(productItem.last())) { 
-      productCurrentItem = nextItem.addClass("active");
-      $(".card").css( {
-        "background": "#ddd",
-      });
-    } else if (nextItem.length) {
-      productCurrentItem = nextItem.addClass("active");
-    } else {
-      productCurrentItem = productItem.first().addClass("active");
-    }
-
-    calcProductHeight();
-    animateContentColor();
-  });
 
   $("#prev").on("click", function(e) {
     e.preventDefault();
@@ -62,7 +43,18 @@ $(document).ready(function() {
 
     productCurrentItem.removeClass("active");
 
-    if (prevItem.length) {
+    if ( prevItem.is(productItem.last().prev() ) ) { 
+      
+      productCurrentItem = prevItem.addClass("active");
+
+      $(".submit").remove();
+
+      var nextButton = $('<a></a>').addClass('btn') 
+          .attr('id',"next").attr('href','#').text('next');
+
+      $(".footer").append(nextButton);
+
+    } else if (prevItem.length) {
       productCurrentItem = prevItem.addClass("active");
     } else {
       productCurrentItem = productItem.last().addClass("active");
@@ -72,11 +64,37 @@ $(document).ready(function() {
     animateContentColor();
   });
 
+  $("#next").on("click", function(e) {
+    e.preventDefault();
+
+    var nextItem = productCurrentItem.next();
+ 
+    productCurrentItem.removeClass("active");
+
+    if ( nextItem.is(productItem.last()) ) { 
+      
+      productCurrentItem = nextItem.addClass("active");
+      $("#next").remove();
+
+      var submitButton = $('<input type="submit"></input').text("SUBMIT").addClass("ui button btn submit");
+      $(".footer").append(submitButton);
+
+    } else if (nextItem.length) {
+      productCurrentItem = nextItem.addClass("active");
+    } else {
+      productCurrentItem = productItem.first().addClass("active");
+    }
+
+    calcProductHeight();
+    animateContentColor();
+
+  });
+
   $(".search").on("click", function() {
     var selected = $(".active .label").length;
     var currentHeight = getProductHeight;
     $(".products").css({
-      "height": currentHeight + 13*selected + "px",
+      "height": currentHeight + 18*selected + "px",
     });
   });
 
