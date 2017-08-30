714e0fe01ce4bb1084363e7aa388d01021d1132f
lylpasko <lyle.paskowitz@gmail.com>
2017-08-30 18:51:32 -0400

    rearranged stylesheets

--- planner/src/components/Dashboard/InfoModule.js
+++ planner/src/components/Dashboard/InfoModule.js
@@ -9,46 +9,60 @@ class InfoModule extends React.Component {
     render() {
         return (
             <div className="module">
-                  <div className="label">
+                  <div className="courseLabel">
                     <div className="course-title">Discrete Mathematics</div>
-                    <div className="label-id">
+                    <div className="courseLabel-id">
                       <p className="course-number">EN.550.171</p>
                       <p className="course-credit">4 credits</p>
                     </div>
                   </div>
                   
-                  <div className="label">
+                  <div className="courseLabel">
                     <div className="course-title">Linear Algebra</div>
-                    <div className="label-id">
+                    <div className="courseLabel-id">
                       <p className="course-number">AS.110.201</p>
                       <p className="course-credit">4 credits</p>
                     </div>
                   </div>
                   
-                  <div className="label">
+                  <div className="courseLabel">
                     <div className="course-title">Nomads, Tyrants, and Kings: Water in the Ancient Near East</div>
-                    <div className="label-id">
+                    <div className="courseLabel-id">
                       <p className="course-number">AS.110.201</p>
                       <p className="course-credit">4 credits</p>
                     </div>
                   </div>
                   
-                  <div className="label"></div>
-                  
-                  <div className="label">
-                    <div className="course-title">Differential Equations</div>
-                    <div className="label-id">
-                      <p className="course-number">AS.110.312</p>
+                  <div className="courseLabel">
+                    <div className="course-title">Intro to Optimization</div>
+                    <div className="courseLabel-id">
+                      <p className="course-number">AS.550.310</p>
                       <p className="course-credit">4 credits</p>
                     </div>
                   </div>
                   
-                  <div className="label">
-                    
+                  <div className="courseLabel">
+                    <div className="course-title">Differential Equations</div>
+                    <div className="courseLabel-id">
+                      <p className="course-number">AS.110.302</p>
+                      <p className="course-credit">4 credits</p>
+                    </div>
                   </div>
                   
-                  <div className="label">
-                    
+                  <div className="courseLabel">
+                    <div className="course-title">Intro to Probability</div>
+                    <div className="courseLabel-id">
+                      <p className="course-number">AS.550.420</p>
+                      <p className="course-credit">4 credits</p>
+                    </div>
+                  </div>
+
+                  <div className="courseLabel">
+                    <div className="course-title">Data Science</div>
+                    <div className="courseLabel-id">
+                      <p className="course-number">AS.600.430</p>
+                      <p className="course-credit">4 credits</p>
+                    </div>
                   </div>
                   
             </div>
