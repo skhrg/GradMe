3a12f78dabd8ac40ff74d41b4e8c93dba2bda9c2
Arsen Klyuev <akliouev1@gmail.com>
2017-11-13 22:06:40 -0500

    Put in placeholder majors/minors in Overview tab

--- planner/src/components/Dashboard/CreditsLeft.js
+++ planner/src/components/Dashboard/CreditsLeft.js
@@ -8,10 +8,10 @@ class CreditsLeft extends React.Component {
         return (
             <div className='progression'>
                 <h1>You need...</h1>
-                    <h2><span style = {{color: "#1e88e5"}}> 40 </span> more credits to complete your major</h2>
-                    <h2><span style = {{color: "#d81b60"}}> 23 </span> more credits to complete your first minor</h2>
-                    <h2><span style = {{color: "#f4511e"}}> 4 </span> more credits to complete your second minor</h2>
-                    <h2><span style = {{color: "#fdd835"}}> 28 </span> more credits to complete your track</h2>
+                    <h2><span style = {{color: "#1e88e5"}}> 40 </span> more credits to complete your Physics major</h2>
+                    <h2><span style = {{color: "#d81b60"}}> 23 </span> more credits to complete your Bioethics minor</h2>
+                    <h2><span style = {{color: "#f4511e"}}> 4 </span> more credits to complete your Bioethics minor</h2>
+                    <h2><span style = {{color: "#fdd835"}}> 28 </span> more credits to complete Pre-Medical track</h2>
             </div>
         );
     }
