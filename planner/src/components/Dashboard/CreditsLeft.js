18c143334af2e44bf286f3eb7fa4b3744668edc4
Arsen Klyuev <akliouev1@gmail.com>
2017-11-13 22:29:09 -0500

    Fixed from E&M to Economics

--- planner/src/components/Dashboard/CreditsLeft.js
+++ planner/src/components/Dashboard/CreditsLeft.js
@@ -10,7 +10,7 @@ class CreditsLeft extends React.Component {
                 <h1>You need...</h1>
                     <h2><span style = {{color: "#1e88e5"}}> 40 </span> more credits to complete the Physics major</h2>
                     <h2><span style = {{color: "#d81b60"}}> 23 </span> more credits to complete the Bioethics minor</h2>
-                    <h2><span style = {{color: "#f4511e"}}> 4 </span> more credits to complete the Entrepreneurship & Management minor</h2>
+                    <h2><span style = {{color: "#f4511e"}}> 4 </span> more credits to complete the Economics minor</h2>
                     <h2><span style = {{color: "#fdd835"}}> 28 </span> more credits to complete the Pre-Medical track</h2>
             </div>
         );
