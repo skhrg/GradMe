9a3a6083313958d4d604d5c4ac7d45979520cca4
lylpasko <lyle.paskowitz@gmail.com>
2018-01-13 17:05:54 -0500

    file restructure

--- Back-End/django/models.py
+++ Back-End/django/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-class SpecificRequirements(models.Model):
+class Requirement(model.Model)
+    specific_requirements = ArrayField(
+        models.ForeignKey(SpecificRequirement, on_delete=models.CASCADE),
+    )
+    generic_requirements = ArrayField(
+        models.ForeignKey(GenericRequirement, on_delete=models.CASCADE),
+    )
+
+class SpecificRequirement(models.Model):
     total_credits = models.IntegerField()
     required_credits = models.IntegerField()
     courses = ArrayField(
         models.ForeignKey(, on_delete=models.CASCADE),
     )
+
+class GenericRequirement(model.Model):
+    total_credits = models.IntegerField()
+    distribution_requirements = models.CharField()
+    department = ArrayField(
+        models.CharField(),
+    )
+    level = models.IntegerField()
+    min_course_credites = models.IntegerField()
+    school = models.CharField()
