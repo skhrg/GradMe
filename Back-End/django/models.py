f5f21c127587a23e03ae4e2c441a7fec722c344b
Saianeesh Keshav Haridas <saianeeshkh@gmail.com>
2018-01-13 16:11:09 -0500

    Made GenericRequirement model

--- Back-End/django/models.py
+++ Back-End/django/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-class SpecificRequirements(models.Model):
+class SpecificRequirement(models.Model):
     total_credits = models.IntegerField()
     required_credits = models.IntegerField()
     courses = ArrayField(
         models.ForeignKey(, on_delete=models.CASCADE),
     )
+
+class GenericRequirement(model.Model):
+    total_credits = models.IntegerField()
+    distribution_requirements = models.CharField()
+    department = ArrayField(
+        models.CharField(),
+    )
+    level = models.IntegerField()
+    min_course_credites = models.IntegerField()
+    school = models.CharField()
