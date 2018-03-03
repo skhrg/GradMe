53bbe4cd7c95b6604aa70bac9118c2be5f621373
lordskh <saianeeshkh@gmail.com>
2018-03-02 20:19:17 -0500

    Added school name to course model

---
+++ courseData/scrapeJHU/scrapeJHU/management/commands/scrape.py
@@ -0,0 +1,5 @@
+import json
+from django.core.management.base import BaseCommand
+from scrapeJHU.models import Course
+
+class Command(BaseCommand)
--- courseData/scrapeJHU/scrapeJHU/models.py
+++ courseData/scrapeJHU/scrapeJHU/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Course(model.Model):
+	schoolName = model.CharField()
 	school = model.IntegerField()
 	department = model.IntegerField()
 	number = model.IntegerField()
