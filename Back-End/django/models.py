fa8c7f65d6542fc77f95d751ca4524c7cbd85de2
lordskh <saianeeshkh@gmail.com>
2018-07-12 17:25:07 -0400

    Streamlined GenericRequirement and added documentation for GenericRequirement

--- Back-End/django/models.py
+++ Back-End/django/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-class Requirement(model.Model)
+class Requirement(model.Model):
     specific_requirements = ArrayField(
         models.ForeignKey(SpecificRequirement, on_delete=models.CASCADE),
     )
@@ -17,11 +17,16 @@ class SpecificRequirement(models.Model):
     )
 
 class GenericRequirement(model.Model):
+	//Total number of credits required for this requirement
     total_credits = models.IntegerField()
-    distribution_requirements = models.CharField()
+	//6-bit integer that encodes the required dist requirements
+	//[W][H][S][N][Q][E]
+    distribution_requirements = models.IntegerField()
+	//List of departments that this requirment can be fullfilled in
     department = ArrayField(
-        models.CharField(),
+        models.IntegerField(),
     )
+	//Minimum level of course
     level = models.IntegerField()
-    min_course_credites = models.IntegerField()
-    school = models.CharField()
+	//Minumum number of credits that a course must be
+    min_course_credits = models.IntegerField()
