18213f53207ab463fbe655cd2a14d73586f829cc
Saianeesh Keshav Haridas <saianeeshkh@gmail.com>
2018-08-16 06:43:25 -0400

    Fixed formatting

--- Back-End/django/models.py
+++ Back-End/django/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-frin sympy import *
+from sympy import *
 
 class Requirement(model.Model):
-    specific_requirements = models.ManyToManyField(SpecificRequirement)
-    generic_requirements = models.ManyToManyField(GenericRequirement)
+	specific_requirements = models.ManyToManyField(SpecificRequirement)
+	generic_requirements = models.ManyToManyField(GenericRequirement)
 
 	#Check if requirement is met
 	def check_req(models.ManyToManyField courselist): #TODO maybe return list of courses used to fullfill req and credit progression
@@ -19,9 +19,9 @@ class Requirement(model.Model):
 		return false
 
 class SpecificRequirement(models.Model):
-    total_credits = models.IntegerField()
-    required_credits = models.IntegerField()
-    courses = models.ManyToManyField(Course)
+	total_credits = models.IntegerField()
+	required_credits = models.IntegerField()
+	courses = models.ManyToManyField(Course)
 
 	#Check if requirement is met
 	def check_req(models.ManyToManyField courselist): #TODO maybe return list of courses used to fullfill req and credit progression
@@ -36,18 +36,18 @@ class SpecificRequirement(models.Model):
 
 class GenericRequirement(model.Model):
 	#Total number of credits required for this requirement
-    total_credits = models.IntegerField()
+	total_credits = models.IntegerField()
 	#n-bit integer that encodes the required dist requirements
 	#[W][H][S][N][Q][E] for JHU
-    distribution_requirements = models.IntegerField()
+	distribution_requirements = models.IntegerField()
 	#List of departments that this requirment can be fullfilled in
-    department = ArrayField(
-        models.IntegerField(),
-    )
+	department = ArrayField(
+		models.IntegerField(),
+	)
 	#Minimum level of course
-    level = models.IntegerField()
+	level = models.IntegerField()
 	#Minumum number of credits that a course must be
-    min_course_credits = models.IntegerField()
+	min_course_credits = models.IntegerField()
 
 	#Check if requirment is met
 	def check_req(models.ManyToManyField courselist): #TODO maybe return list of courses used to fullfill req and credit progression
